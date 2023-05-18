aee17af2c93cd275c8b4ecac30c7da79f7accc80
marian <82874011+DiabolicWitch@users.noreply.github.com>
2023-05-17 21:33:19 -0300

    Add files via upload

--- slaking.py
+++ slaking.py
@@ -1,8 +1,10 @@
 import sys
 import getpass
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QTabWidget, QListWidget, QDialog, QDialogButtonBox, QMessageBox, QSizePolicy, QSpacerItem
+import json
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QTabWidget, QListWidget, QDialog, QDialogButtonBox, QMessageBox
 
-class NewListDialog(QDialog):
+class NewList(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle("Slaking criará uma lista!")
@@ -15,36 +17,41 @@ class NewListDialog(QDialog):
         button_box.rejected.connect(self.reject)
         self.layout.addWidget(button_box)
         self.setLayout(self.layout)
+        # Conectar evento closeEvent ao método delete_checked_items
+        self.closeEvent = lambda event: self.delete_checked_items()
 
     def get_list_name(self):
         return self.new_list_name_input.text()
+    # Responsavel por pegar o nome da nova lista na classe e retorna-la para ser aderida na janela
+    
 
 
-class ToDoList(QWidget):
+class Slaking(QWidget):
     def __init__(self):
         super().__init__()
         self.lists = {}
         self.setup_ui()
+        # Conecta a função update_current_list na troca de abas
+        self.tab_widget.currentChanged.connect(self.update_current_list)
 
     def setup_ui(self):
         self.layout = QVBoxLayout()
 
         self.tab_widget = QTabWidget()
 
-        self.general_list_widget = QListWidget()
-        self.tab_widget.addTab(self.general_list_widget, "Geral")
-        self.lists["Geral"] = self.general_list_widget
-
-        self.layout.addWidget(self.tab_widget)
-
-        self.button_layout = QHBoxLayout()
+        self.button_container = QWidget()
+        self.button_layout = QHBoxLayout(self.button_container)
+        self.button_layout.setAlignment(Qt.AlignLeft)
+        # Botões de nova lista e remover lista
         self.new_list_button = QPushButton("+")
         self.new_list_button.setFixedSize(23, 23)
-        self.tab_widget.setCornerWidget(self.new_list_button)      
-
         self.remove_list_button = QPushButton("-")
         self.remove_list_button.setFixedSize(23, 23)
-        self.tab_widget.setCornerWidget(self.remove_list_button)
+        self.button_layout.addWidget(self.new_list_button)
+        self.button_layout.addWidget(self.remove_list_button)
+
+        self.layout.addWidget(self.tab_widget)
+        self.layout.addWidget(self.button_container)
 
         self.add_item_widget = QWidget()
         self.add_item_layout = QHBoxLayout()
@@ -62,13 +69,24 @@ class ToDoList(QWidget):
 
         self.setLayout(self.layout)
 
+        # Conecta as funções aos botões
         self.new_list_button.clicked.connect(self.show_new_list_dialog)
-        self.remove_list_button.clicked.connect(self.remove_list)
         self.add_item_button.clicked.connect(self.add_item)
+        self.remove_list_button.clicked.connect(self.remove_list)
         self.add_item_name_input.returnPressed.connect(self.add_item)
 
+        # Parte do código responsável por fazer o evento doubleclick marcar o item da lista
+        def connect_item_double_clicked():
+            current_list_widget = self.tab_widget.currentWidget()
+            if current_list_widget is not None:
+                current_list_widget.itemDoubleClicked.connect(self.toggle_item_strikeout)
+        
+        self.tab_widget.currentChanged.connect(connect_item_double_clicked)
+        connect_item_double_clicked()
+
+    # Chama a classe NewList para criar uma nova lista e chama add_list para que ela seja adicionada as abas
     def show_new_list_dialog(self):
-        dialog = NewListDialog(self)
+        dialog = NewList(self)
         if dialog.exec_() == QDialog.Accepted:
             list_name = dialog.get_list_name()
             self.add_list(list_name)
@@ -86,13 +104,21 @@ class ToDoList(QWidget):
             message_box.setDefaultButton(QMessageBox.No)
             if message_box.exec_() == QMessageBox.Yes:
                 self.tab_widget.removeTab(current_tab_index)
+                self.lists.pop(current_list_name)  # Remove a lista do dicionário
 
+                # Remove a lista do arquivo JSON
+                with open("lists.json", "r") as file:
+                    data = json.load(file)
+                if current_list_name in data:
+                    data.pop(current_list_name)
+                    with open("lists.json", "w") as file:
+                        json.dump(data, file)
 
     def add_list(self, list_name):
-        if list_name and list_name not in self.lists:
+        if list_name not in self.lists:
             list_widget = QListWidget()
-            self.tab_widget.addTab(list_widget, list_name)
-            self.lists[list_name] = list_widget
+            self.tab_widget.addTab(list_widget, list_name) # Adiciona aba 
+            self.lists[list_name] = list_widget 
 
     def add_item(self):
         current_tab_index = self.tab_widget.currentIndex()
@@ -100,13 +126,66 @@ class ToDoList(QWidget):
         item_name = self.add_item_name_input.text()
         if current_list_widget is not None and item_name:
             current_list_widget.addItem(" - " + item_name.capitalize())
+            self.save_lists()
         self.add_item_name_input.clear()
 
+    def toggle_item_strikeout(self, item):
+        font = item.font()
+        if font.strikeOut():
+            font.setStrikeOut(False)
+        else:
+            font.setStrikeOut(True)
+        item.setFont(font)
+
+        # Remove os itens marcados da lista quando o programa for fechado
+        app = QApplication.instance()
+        app.aboutToQuit.connect(self.remove_marked_items)
+
+    def update_current_list(self, index):
+        current_list_widget = self.tab_widget.currentWidget()
+        if current_list_widget is not None:
+            # Limpa a conexão com o sinal itemDoubleClicked para evitar duplicação
+            current_list_widget.itemDoubleClicked.disconnect()
+            # Conecta o sinal itemDoubleClicked com a função toggle_item_strikeout
+            current_list_widget.itemDoubleClicked.connect(self.toggle_item_strikeout)
+    
+    def remove_marked_items(self):
+        for list_widget in self.lists.values():
+            for i in reversed(range(list_widget.count())):
+                item = list_widget.item(i)
+                if item.font().strikeOut():
+                    list_widget.takeItem(i)
+        self.save_lists()
+
+    def save_lists(self):
+        data = {}
+        for list_name, list_widget in self.lists.items():
+            items = [list_widget.item(i).text() for i in range(list_widget.count())]
+            data[list_name] = items
+
+        with open("lists.json", "w") as file:
+            json.dump(data, file)
+
+    def load_lists(self):
+        try:
+            with open("lists.json", "r") as file:
+                data = file.read()
+                if data:
+                    data = json.loads(data)
+                    for list_name, items in data.items():
+                        list_widget = QListWidget()
+                        for item in items:
+                            list_widget.addItem(item)
+                        self.tab_widget.addTab(list_widget, list_name)
+                        self.lists[list_name] = list_widget
+        except (FileNotFoundError, json.JSONDecodeError):
+            pass
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = QMainWindow()
-    todo_list = ToDoList()
+    todo_list = Slaking()
+    todo_list.load_lists() # Carrega as listas salvas anteriormente
     window.setCentralWidget(todo_list)
     user = getpass.getuser()
     window.setWindowTitle(f"Slaking de {user}")
